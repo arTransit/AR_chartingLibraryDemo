d2fc0e6fe1be31cb491f27ca5dd9651ceb4f5128
arTransit <andrew_ross@bctransit.com>
2013-09-16 09:34:16 -0700

    removed/renamed files

--- html/chartingGlue.js
+++ html/chartingGlue.js
@@ -27,28 +27,14 @@ var columnNames = [];               // name of each data series
 var annotations = [];               // array of annotation (date, label)
 var BASEURL = "http://localhost:8080/chartdata/";   // url for dataProvider
 var lineColours = [];               // array of colours for each line/series
-var flotChart, flotSmallChart;      // pointers to charts
-var yMin, yMax;                     // max extents of flotChart y-axis (only used to reset zoom)
-var xMin, xMax;                     // max extents of flotChart x-axis (used to reset extent)
-
+var flotChart = {};                 // pointers to charts
 
 /*************************************************************************
  * Chart functions
  *
  */
 
-function setChartExtents( min, max ) {
-    var xaxis = flotChart.getAxes().xaxis;
-    xaxis.options.min = min;
-    xaxis.options.max = max;
-    var yaxis = flotChart.getAxes().yaxis;
-    yaxis.options.min = yMin;
-    yaxis.options.max = yMax;
 
-    updateDateRangeBox( min, max);
-    flotChart.setupGrid();
-    flotChart.draw();
-}
 
 function updateDateRangeBox( fromDate,toDate ) {
     $('#dateRangeBox').val( 
@@ -94,7 +80,6 @@ function updateChartData( chartType ) {
                            '#003203','#00990B','#00FF12','#65FF70'];
             break;
     }
-    
     getGFIdata( url ).done( function() {
         buildChart( rowData, columnNames, annotations );
     });
@@ -108,8 +93,8 @@ function updateChartData( chartType ) {
 function buildChart( rowData, columnNames, annotations ) {
     var flotOptions = {
         legendlist:{
-           legendDiv: "chartLegend",
-           legendTitle: "My New Legend"
+           legendTitle: "Legend",
+           legendDiv: "chartLegend"
         },
         legend: {
             show:false
@@ -124,117 +109,14 @@ function buildChart( rowData, columnNames, annotations ) {
         xaxis: { 
             mode: "time", 
             minTickSize: [1, "day"],
-            //timeformat: "%e %b %Y",
-            font: { size: 10, family: "sans-serif", color: "#000" },
-            ticks:generateXaxisTicks,
-            tickFormatter: xAxisDateFormat
+            font: { size: 10, family: "sans-serif", color: "#000" }
         },
-        yaxis: {
-            reserveSpace: 50,
-            tickFormatter: numberWithCommas
-        },
-        colors: lineColours,
-
-        /*
-         * Remove chart slippy map behaviour
-         *
-        zoom: {
-            interactive: true
-        },
-        pan: {
-            interactive: true
-        },
-         *
-         */
-
-        grid: {
-            hoverable: true,   // needed for tooltip
-            autoHighlight: true
-        }
+        yaxis: {},
+        colors: lineColours
     }
     
-    flotChart = $.plot("#flotchart", rowData, flotOptions );
-    yMin = flotChart.getAxes().yaxis.min;
-    yMax = flotChart.getAxes().yaxis.max;
-    xMin = flotChart.getAxes().xaxis.min;
-    xMax = flotChart.getAxes().xaxis.max;
-    
-    //draw smallchart
-    var sData = $.extend(true,[],rowData);
-    for(var i=0;i<sData.length;i++){
-        sData[i].color = '#2E2E2E';
-        sData[i].label = undefined;
-    }
-    flotsmallchart = $.plot("#flotsmallchart",sData,{
-        xaxis: {
-            mode: "time", 
-            minTickSize: [6, "month"],
-            timeformat: "%b %Y",
-            font: { size: 8, weight: "bold", family: "sans-serif", color: "#000" }
-        },
-        yaxis: {
-            reserveSpace: 50,
-            show: false
-        },
-        grid:{
-            color: "#666"
-        }
-    });
-
-
-
-    /* setup annotations
-     */
-
-    // displayAnnotations( flotChart.getAxes().xaxis.min, flotChart.getAxes().xaxis.max ); 
-
-
-    /* setup slider
-     */
-
-    var xaxis = flotChart.getAxes().xaxis;
-    //console.log("min:" + xaxis.min);
-    //console.log("max:" + xaxis.max);
-    $("#slider").dateRangeSlider({
-        defaultValues:{min: new Date(xaxis.min),max: new Date(xaxis.max)},
-        bounds:{min: new Date(xaxis.min),max: new Date(xaxis.max)},
-        formatter:function(val){
-            var days = val.getDate(),
-                year = val.getFullYear();
-            return year + " " 
-                    + ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
-                       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][val.getMonth()] 
-                    + " " + days;
-        }
-    });
-    $("#slider").on("valuesChanging", function(e, data){
-        setChartExtents( data.values.min, data.values.max );
-    });
-}
-
-
-/* function numberWithCommas( n )
- *      given integer n, return string of number commas between thousands
- *      1234567 -> 1,234,567
- */
-function numberWithCommas(n) {
-    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function xAxisDateFormat( n ) {
-    return "<span class=\"rotatedLable\">"
-            + $.format.date(n, "yyyy MMM d") + "</span>";
-}
-
-function generateXaxisTicks( axis ) {
-    var ticks=[], 
-        t=0,
-        sp=(axis.max-axis.min)/5;
-    do {
-        ticks.push( t * sp );
-        ++t;
-    } while ( (t * sp) < axis.max);
-    return ticks;
+    flotChart = new BiCharting.fullChart( flotOptions, rowData, "mainChart" );
+ 
 }
 
 
@@ -247,12 +129,11 @@ function setExtent( id ) {
     var d = new Date();
     var m = d.getMonth(),
         y = d.getFullYear();
-    console.log("Now: " + m + "  " + y);
 
     switch( id ) {
         case "fullExtent":
-            var min = new Date(xMin),  //use chart extents
-                max = new Date(xMax);
+            var min = new Date(flotChart.xMin),  //use chart extents
+                max = new Date(flotChart.xMax);
             break;
         case "currentYear":
             var min = new Date( y,0,1),
@@ -271,47 +152,9 @@ function setExtent( id ) {
                 max = new Date( y,m,0);
             break;
     }
-    console.log("extent:" + min + " " + max);
-    $("#slider").dateRangeSlider("values", min, max);
-    setChartExtents( min.getTime(), max.getTime() ); 
+    flotChart.setExtents( min, max );
 }
 
-
-/*************************************************************************
- * Annotation functions
- *
- */
-
-/*
-function displayAnnotations( min, max ) {
-    $('#annotationList li').remove();
-    for( var i=0; i< annotations.length; i++){
-        if( annotations[i].max >= min && annotations[i].max <= max ) {
-            $("#annotationList").append(
-                    "<li class=\"annotationItem\" name=\"" + i + "\">"
-                    + "<b>" + annotations[i].title + "</b><br\>"
-                    + annotations[i].description
-                    + "<br\><i>" + $.format.date( annotations[i].max,"ddd, dd MMM yyyy") + "</i>"
-                    + "</li>" );
-        }
-    }
-    
-    //make menu item bold with focus
-    $(".annotationItem").hover( 
-        function() {
-            $(".annotationItem").removeClass("annotationActiveItem");
-            $(".annotationItem").addClass("annotationInactiveItem");
-            $(this).removeClass("annotationInactiveItem");
-            $(this).addClass("annotationActiveItem");
-            flotChart.highlightEvent($(this).attr("name") );
-        }, 
-        function() {
-            $(this).removeClass("annotationActiveItem");
-            $(this).addClass("annotationInactiveItem");
-            flotChart.unhighlightEvent($(this).attr("name") );
-    });
-}
-*/
 
 /*************************************************************************
  * Document functions / jQuery setup
@@ -319,54 +162,10 @@ function displayAnnotations( min, max ) {
  */
 
 $(function() {
-
-/*
-    // setup annotation interactivity
-    $("#annotationList").mouseleave( function() {   //reset annotation when looses focus
-        $(".annotationItem").removeClass("annotationActiveItem");
-        $(".annotationItem").removeClass("annotationInactiveItem");
-    });
-*/
-
-    //pulldown list to change view of data
-    //  All data: date range beginning to end
-    //  Year on year: current year overlapping previous years
     $('#dataView').change(function() {
         updateChartData( $('#dataView').val() );
     });
-    
-
-    // datatooltip box for mouse hover
-    $("<div id='datatooltip'></div>").css({
-        position: "absolute",
-        display: "none",
-        border: "1px solid #fdd",
-        padding: "2px",
-        "background-color": "#fee",
-        opacity: 0.80,
-        "font-family": "sans-serif",
-        //"font-weight": "bold",
-        "font-size": "small"
-    }).appendTo("body");
-    
-   
-    $("#flotchart").bind("plothover", function (event, pos, item) {
-        if (item) {
-            var y = item.datapoint[1],
-                x = item.datapoint[0];
-            $("#datatooltip").html(
-                "<b>" + item.series.label + ": " 
-                + numberWithCommas( y) + "</b>"
-                + "<br/><i>" + $.format.date(x, "yyyy MMM d") +"</i>"
-            ).css({top: item.pageY+5, left: item.pageX+5})
-            .fadeIn(200);
-        } else {
-            $("#datatooltip").hide();
-        }
-    });
-
     updateChartData( $('#dataView').val() );
-
 });
 
 
